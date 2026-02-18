e9376574d80a5667d029769c16b2df5efd4451dc
Ankush Gupta <Officialankushguptaa@gmail.com>
2026-02-18 10:58:43 +0530

    Create roman_to_integer.py

---
+++ roman_to_integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        output = 0
+        for i in range(len(s)):
+            element= roman[s[i]]
+            next_element= roman[s[i+1]] if i+1 < len(s) else 0
+
+            if element < next_element:
+                output-=element
+            else:
+                output+=element
+        return output
+
